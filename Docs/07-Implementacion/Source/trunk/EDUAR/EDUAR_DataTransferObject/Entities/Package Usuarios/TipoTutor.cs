95445eb64681883624f8d60646fe32312a5387b1
king.orco <king.orco@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2011-07-04 23:12:25 +0000

    upssssssssssssssss, me comi esta clase

---
+++ Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_DataTransferObject/Entities/Package Usuarios/TipoTutor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EDUAR_Entities.Shared;
+
+namespace EDUAR_Entities
+{
+    public class TipoTutor : DTBase
+    {
+        private Decimal _idTipoTutor;
+
+        public Decimal idTipoTutor
+        {
+            get { return _idTipoTutor; }
+            set { _idTipoTutor = value; }
+        }
+
+        private Decimal _idTipoTutorTransaccional;
+
+        public Decimal idTipoTutorTransaccional
+        {
+            get { return _idTipoTutorTransaccional; }
+            set { _idTipoTutorTransaccional = value; }
+        }
+
+        private string _descripcion;
+
+        public string descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+
+    }
+}
