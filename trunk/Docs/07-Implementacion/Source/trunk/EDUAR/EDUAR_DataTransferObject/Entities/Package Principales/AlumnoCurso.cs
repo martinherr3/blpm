dad6dc60c151b61f0c52400ad09703d0619063e7
laupastorino@gmail.com <laupastorino@gmail.com@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2011-07-23 16:45:02 +0000

    Lau - Clase AlumnoCurso.cs

---
+++ trunk/Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_DataTransferObject/Entities/Package Principales/AlumnoCurso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EDUAR_Entities.Shared;
+
+namespace EDUAR_Entities
+{
+    public class AlumnoCurso : DTBase
+    {
+        public int idAlumnoCurso { get; set; }
+        public int idAlumnoCursoTransaccional { get; set; }
+        public Alumno alumno { get; set; }
+        public Curso curso { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AlumnoCurso"/> class.
+        /// </summary>
+        public AlumnoCurso()
+        {
+            idAlumnoCurso = 0;
+            idAlumnoCursoTransaccional = 0;
+            alumno = new Alumno();
+            curso = new Curso();
+        }
+    }
+}
