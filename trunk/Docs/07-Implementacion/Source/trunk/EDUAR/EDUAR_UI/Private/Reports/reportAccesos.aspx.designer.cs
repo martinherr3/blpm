190c441292e160a3b0e3d3ce948af6b29b4227fe
laupastorino@gmail.com <laupastorino@gmail.com@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2011-07-24 14:06:43 +0000

    Lau - Reportes

--- trunk/Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_UI/Private/Reports/reportAccesos.aspx.designer.cs
+++ trunk/Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_UI/Private/Reports/reportAccesos.aspx.designer.cs
@@ -40,6 +40,15 @@ namespace EDUAR_UI {
         protected global::System.Web.UI.WebControls.DropDownList ddlPagina;
         
         /// <summary>
+        /// Control udpReporte.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel udpReporte;
+        
+        /// <summary>
         /// Control rptAccesos.
         /// </summary>
         /// <remarks>
