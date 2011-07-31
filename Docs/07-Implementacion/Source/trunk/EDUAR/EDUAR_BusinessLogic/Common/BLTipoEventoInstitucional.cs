bb73984325c951454bc793e309cb4688eabe3bfe
king.orco <king.orco@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2011-07-31 21:18:44 +0000

    Clases de Tipo de evento institucional

--- Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_BusinessLogic/Common/BLTipoEventoInstitucional.cs
+++ Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_BusinessLogic/Common/BLTipoEventoInstitucional.cs
@@ -164,22 +164,22 @@ namespace EDUAR_BusinessLogic.Common
         #endregion
 
         #region --[Métodos publicos]--
-        //public List<TipoEventoInstitucional> GetTipoEventoInstitucionals(TipoEventoInstitucional entidad)
-        //{
-        //    try
-        //    {
-        //        return DataAcces.GetTipoEventoInstitucionals(entidad);
-        //    }
-        //    catch (CustomizedException ex)
-        //    {
-        //        throw ex;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw new CustomizedException(string.Format("Fallo en {0} - GetTipoEventoInstitucionals", ClassName), ex,
-        //                                      enuExceptionType.BusinessLogicException);
-        //    }
-        //}
+        public List<TipoEventoInstitucional> GetTipoEventoInstitucional(TipoEventoInstitucional entidad)
+        {
+            try
+            {
+                return DataAcces.GetTipoEventoInstitucional(entidad);
+            }
+            catch (CustomizedException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - GetTipoEventoInstitucionals", ClassName), ex,
+                                              enuExceptionType.BusinessLogicException);
+            }
+        }
         #endregion
     }
 }
--- Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_DataAccess/Common/DATipoEventoInstitucional.cs
+++ Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_DataAccess/Common/DATipoEventoInstitucional.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using EDUAR_DataAccess.Shared;
 using EDUAR_Entities;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using EDUAR_Utility.Excepciones;
+using EDUAR_Utility.Enumeraciones;
+using System.Data;
 
 namespace EDUAR_DataAccess.Common
 {
@@ -35,7 +40,35 @@ namespace EDUAR_DataAccess.Common
 
         public override TipoEventoInstitucional GetById(TipoEventoInstitucional entidad)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Transaction.DBcomand = Transaction.DataBase.GetStoredProcCommand("TipoEventoInstitucional_Select");
+
+                if (entidad.idTipoEventoInstitucional > 0)
+                    Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idTipoEventoInstitucional", DbType.Int32, entidad.idTipoEventoInstitucional);
+
+                IDataReader reader = Transaction.DataBase.ExecuteReader(Transaction.DBcomand);
+
+                TipoEventoInstitucional objTipoEvento = new TipoEventoInstitucional();
+
+                while (reader.Read())
+                {
+                    objTipoEvento.idTipoEventoInstitucional = (int)(reader["idTipoEventoInstitucional"]);
+                    objTipoEvento.descripcion = (string)(reader["descripcion"]);
+                    objTipoEvento.activo = (bool)(reader["activo"]);
+                }
+                return objTipoEvento;
+            }
+            catch (SqlException ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - GetTipoEvento()", ClassName),
+                                    ex, enuExceptionType.SqlException);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - GetTipoEvento()", ClassName),
+                                    ex, enuExceptionType.DataAccesException);
+            }
         }
 
         public override void Create(TipoEventoInstitucional entidad)
@@ -60,6 +93,48 @@ namespace EDUAR_DataAccess.Common
         #endregion
 
         #region --[Métodos Públicos]--
+
+        public List<TipoEventoInstitucional> GetTipoEventoInstitucional(TipoEventoInstitucional entidad)
+        {
+            try
+            {
+                Transaction.DBcomand = Transaction.DataBase.GetStoredProcCommand("EventoInstitucional_Select");
+                if (entidad != null)
+                {
+                }
+
+                IDataReader reader = Transaction.DataBase.ExecuteReader(Transaction.DBcomand);
+
+                List<TipoEventoInstitucional> listTipoEventos = new List<TipoEventoInstitucional>();
+
+                TipoEventoInstitucional objTipoEvento;
+
+                while (reader.Read())
+                {
+                    objTipoEvento = new TipoEventoInstitucional();
+
+                    objTipoEvento.idTipoEventoInstitucional = (int)(reader["idTipoEvento"]);
+                    objTipoEvento.descripcion = (string)(reader["idTipoEvento"]);
+                    objTipoEvento.activo = (bool)(reader["activo"]);
+
+                    if (objTipoEvento.activo) listTipoEventos.Add(objTipoEvento);
+                }
+
+                return listTipoEventos;
+            }
+            catch (SqlException ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - GetTipoEventoInstitucional()", ClassName),
+                                    ex, enuExceptionType.SqlException);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - GetTipoEventoInstitucional()", ClassName),
+                                    ex, enuExceptionType.DataAccesException);
+            }
+            
+        }
+
         #endregion
     }
 }
