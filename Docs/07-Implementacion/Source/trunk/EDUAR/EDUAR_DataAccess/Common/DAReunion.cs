31079bc51d06f0afbf2ca1057705f5d6cbbde4ed
king.orco <king.orco@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2011-08-16 02:51:41 +0000

    Cambios en clases de evento Reunion, además se actualizó el SP en la base de datos

--- Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_DataAccess/Common/DAReunion.cs
+++ Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_DataAccess/Common/DAReunion.cs
@@ -40,35 +40,7 @@ namespace EDUAR_DataAccess.Common
 
 		public override Reunion GetById(Reunion entidad)
 		{
-			try
-			{
-				Transaction.DBcomand = Transaction.DataBase.GetStoredProcCommand("Reunion_Select");
-
-				if (entidad.idReunion > 0)
-					Transaction.DataBase.AddInParameter(Transaction.DBcomand, "idReunion", DbType.Int32, entidad.idReunion);
-
-				IDataReader reader = Transaction.DataBase.ExecuteReader(Transaction.DBcomand);
-
-				Reunion objReunion = new Reunion();
-				while (reader.Read())
-				{
-					objReunion.idReunion = Convert.ToInt32(reader["idReunion"]);
-					objReunion.horario = Convert.ToDateTime(reader["horario"].ToString());
-					objReunion.idEventoAgenda = Convert.ToInt32(reader["idEvento"]);
-
-				}
-				return objReunion;
-			}
-			catch (SqlException ex)
-			{
-				throw new CustomizedException(string.Format("Fallo en {0} - GetReunion()", ClassName),
-									ex, enuExceptionType.SqlException);
-			}
-			catch (Exception ex)
-			{
-				throw new CustomizedException(string.Format("Fallo en {0} - GetReunion()", ClassName),
-									ex, enuExceptionType.DataAccesException);
-			}
+            throw new NotImplementedException();
 		}
 
 		public override void Create(Reunion entidad)
@@ -77,9 +49,19 @@ namespace EDUAR_DataAccess.Common
 			{
 				Transaction.DBcomand = Transaction.DataBase.GetStoredProcCommand("Reunion_Insert");
 
-				//Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idReunion", DbType.Int32, 0);
+                // Propios del evento de agenda
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idAgendaActividades", DbType.Int32, entidad.idAgendaActividad);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@username", DbType.String, entidad.usuario.username);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idTipoEvento", DbType.Int32, (int)enumEventoAgendaType.Evaluacion);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@fechaModificacion", DbType.Date, DBNull.Value);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@fechaEvento", DbType.Date, entidad.fechaEvento);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@activo", DbType.Boolean, entidad.activo);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@descripcion", DbType.String, entidad.descripcion);
+
+                //Propios de la reunión
+				Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idReunion", DbType.Int32, 0);
 				Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@horario", DbType.DateTime, entidad.horario);
-				//Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idEvento", DbType.Int32, entidad.idEventoAgenda);
+				Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idEvento", DbType.Int32, entidad.idEventoAgenda);
 
 				if (Transaction.Transaction != null)
 					Transaction.DataBase.ExecuteNonQuery(Transaction.DBcomand, Transaction.Transaction);
@@ -100,12 +82,79 @@ namespace EDUAR_DataAccess.Common
 
 		public override void Create(Reunion entidad, out int identificador)
 		{
-			throw new NotImplementedException();
+            try
+            {
+                Transaction.DBcomand = Transaction.DataBase.GetStoredProcCommand("Reunion_Insert");
+
+                // Propios del evento de agenda
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idAgendaActividades", DbType.Int32, entidad.idAgendaActividad);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@username", DbType.String, entidad.usuario.username);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idTipoEvento", DbType.Int32, (int)enumEventoAgendaType.Evaluacion);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@fechaModificacion", DbType.Date, DBNull.Value);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@fechaEvento", DbType.Date, entidad.fechaEvento);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@activo", DbType.Boolean, entidad.activo);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@descripcion", DbType.String, entidad.descripcion);
+
+                //Propios de la reunión
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idReunion", DbType.Int32, 0);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@horario", DbType.DateTime, entidad.horario);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idEvento", DbType.Int32, entidad.idEventoAgenda);
+
+                if (Transaction.Transaction != null)
+                    Transaction.DataBase.ExecuteNonQuery(Transaction.DBcomand, Transaction.Transaction);
+                else
+                    Transaction.DataBase.ExecuteNonQuery(Transaction.DBcomand);
+
+                identificador = Int32.Parse(Transaction.DBcomand.Parameters["@idReunion"].Value.ToString());
+            }
+            catch (SqlException ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - Create()", ClassName),
+                                    ex, enuExceptionType.SqlException);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - Create()", ClassName),
+                                    ex, enuExceptionType.DataAccesException);
+            }
 		}
 
 		public override void Update(Reunion entidad)
 		{
-			throw new NotImplementedException();
+            try
+            {
+                Transaction.DBcomand = Transaction.DataBase.GetStoredProcCommand("Reunion_Update");
+
+                // Propios del evento de agenda
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idAgendaActividades", DbType.Int32, entidad.idAgendaActividad);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@username", DbType.String, entidad.usuario.username);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idTipoEvento", DbType.Int32, (int)enumEventoAgendaType.Evaluacion);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@fechaModificacion", DbType.Date, DBNull.Value);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@fechaEvento", DbType.Date, entidad.fechaEvento);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@activo", DbType.Boolean, entidad.activo);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@descripcion", DbType.String, entidad.descripcion);
+
+                //Propios de la reunión
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idReunion", DbType.Int32, 0);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@horario", DbType.DateTime, entidad.horario);
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idEvento", DbType.Int32, entidad.idEventoAgenda);
+
+                if (Transaction.Transaction != null)
+                    Transaction.DataBase.ExecuteNonQuery(Transaction.DBcomand, Transaction.Transaction);
+                else
+                    Transaction.DataBase.ExecuteNonQuery(Transaction.DBcomand);
+
+            }
+            catch (SqlException ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - Update()", ClassName),
+                                    ex, enuExceptionType.SqlException);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - Update()", ClassName),
+                                    ex, enuExceptionType.DataAccesException);
+            }
 		}
 
 		public override void Delete(Reunion entidad)
@@ -128,7 +177,16 @@ namespace EDUAR_DataAccess.Common
 						Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idEvento", DbType.Int32, entidad.idEventoAgenda);
 					if (entidad.horario != null)
 						Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@horario", DbType.Time, Convert.ToDateTime(entidad.horario).ToShortTimeString());
-				}
+
+                    if (entidad.activo == true)
+                        Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@activo", DbType.Int32, entidad.activo);
+                    if (entidad.fechaEventoDesde != null)
+                        Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@fechaDesde", DbType.Int32, entidad.fechaEventoDesde);
+                    if (entidad.fechaEventoHasta != null)
+                        Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@fechaHasta", DbType.Int32, entidad.fechaEventoHasta);
+                    if (entidad.idAgendaActividad > 0)
+                        Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idAgendaActividad", DbType.Int32, entidad.idAgendaActividad);
+                }
 
 				IDataReader reader = Transaction.DataBase.ExecuteReader(Transaction.DBcomand);
 
@@ -147,10 +205,15 @@ namespace EDUAR_DataAccess.Common
 
                     objEvento.activo = Convert.ToBoolean(reader["activo"]);
                     objEvento.descripcion = reader["descripcion"].ToString();
-                    objEvento.fechaEvento = Convert.ToDateTime(reader["fechaEvento"]);
 
+                    objEvento.fechaEvento = Convert.ToDateTime(reader["fechaEvento"]);
+                    objEvento.fechaAlta = Convert.ToDateTime(reader["fechaAlta"]);
+
+                    objEvento.tipoEventoAgenda = new TipoEventoAgenda { idTipoEventoAgenda = Convert.ToInt32(reader["idTipoEvento"]), descripcion = reader["tipoEvento"].ToString() };
                     objEvento.usuario = new Persona() { idPersona = Convert.ToInt32(reader["idOrganizador"]), nombre = reader["nombreOrganizador"].ToString(), apellido = reader["apellidoOrganizador"].ToString() };
-					
+
+                    objEvento.idAgendaActividad = Convert.ToInt32(reader["idAgendaActividades"]);
+
                     listReuniones.Add(objEvento);
 				}
 				return listReuniones;
