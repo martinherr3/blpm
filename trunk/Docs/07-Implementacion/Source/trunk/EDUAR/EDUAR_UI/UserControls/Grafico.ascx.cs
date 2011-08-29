fdadaa6dd6068164e3af74c70e12b69cd6d636c3
laupastorino@gmail.com <laupastorino@gmail.com@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2011-08-29 17:10:53 +0000

    Lau - Reportes

--- trunk/Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_UI/UserControls/Grafico.ascx.cs
+++ trunk/Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_UI/UserControls/Grafico.ascx.cs
@@ -11,7 +11,7 @@ using EDUAR_UI.Utilidades;
 
 namespace EDUAR_UI.UserControls
 {
-	public partial class Grafico : UserControl
+    public partial class Grafico : UserControl
     {
         private string _ColumnaNombre;
         private string _ColumnaValor;
@@ -79,20 +79,43 @@ namespace EDUAR_UI.UserControls
                 {
                     if (ViewState["_Titulo"] != null)
                     {
-						Chart1.Titles.Clear();
-						Title t = new Title(ViewState["_Titulo"].ToString(), Docking.Top, new System.Drawing.Font("Tahoma", 16, System.Drawing.FontStyle.Underline), System.Drawing.Color.FromArgb(52, 16, 16));
-						Chart1.Titles.Add(t);
+                        Chart1.Titles.Clear();
+                        Title t = new Title(ViewState["_Titulo"].ToString(), Docking.Top, new System.Drawing.Font("Helvetica Neue", 16, System.Drawing.FontStyle.Bold), System.Drawing.Color.Black);
+                        Chart1.Titles.Add(t);
                     }
                 }
                 return "";
             }
             set
             {
-                Chart1.Titles.Clear();
-                Title t = new Title(value, Docking.Top, new System.Drawing.Font("Tahoma", 16, System.Drawing.FontStyle.Underline), System.Drawing.Color.FromArgb(52, 16, 16));
-                Chart1.Titles.Add(t);
                 ViewState["_Titulo"] = value;
             }
+        }
+
+        public List<MiSerie> ListaSeries
+        {
+            get
+            {
+                if (ViewState["_Series"] == null)
+                    ListaSeries = new List<MiSerie>();
+                return (List<MiSerie>)ViewState["_Series"];
+            }
+            set { ViewState["_Series"] = value; }
+        }
+        
+        [Serializable]
+        public class MiSerie
+        {
+            public DataTable Datos;
+            public string ColumnaValor;
+            public string ColumnaNombre;
+            public string NombreSerie;
+
+            public MiSerie()
+            { }
+
+            ~MiSerie()
+            { }
         }
 
         public Chart ChartTesis
@@ -119,24 +142,39 @@ namespace EDUAR_UI.UserControls
                 {
                     divGrafico.Visible = true;
                 }
-				//divNoHayDatos.Visible = false;
+                //divNoHayDatos.Visible = false;
             }
             else
             {
                 divGrafico.Visible = false;
-				//divNoHayDatos.Visible = true;
+                //divNoHayDatos.Visible = true;
             }
         }
 
         private void Graficar()
         {
-            Chart1.Series["Default"].Points.DataBind(Datos.DefaultView, ColumnaNombre, ColumnaValor, "");
-			Chart1.Titles.Add(Titulo);
+            foreach (MiSerie item in ListaSeries)
+            {
+                Chart1.Series[item.NombreSerie].Points.DataBind(item.Datos.DefaultView, item.ColumnaNombre, item.ColumnaValor, "");
+            }
+            //Chart1.Series["Default"].Points.DataBind(Datos.DefaultView, ColumnaNombre, ColumnaValor, "");
+            Chart1.Titles.Add(Titulo);
             _IsListado = false;
-			//divGrafico.Visible = true;
-			divGrafico.Attributes.Add("class", "divGraficoMostrar");
-			TDGrafico.Attributes.Add("class", "divGraficoMostrar");
-			TDBotonera.Attributes.Add("class", "BotoneraGraficoMostrar");
+            //divGrafico.Visible = true;
+            divGrafico.Attributes.Add("class", "divGraficoMostrar");
+            TDGrafico.Attributes.Add("class", "divGraficoMostrar");
+            TDBotonera.Attributes.Add("class", "BotoneraGraficoMostrar");
+        }
+
+        public void AgregarSerie(string serie, DataTable dt, string ColumnaNombre, string ColumnaValor)
+        {
+            MiSerie serieNueva = new MiSerie();
+            serieNueva.NombreSerie = serie;
+            serieNueva.Datos = dt;
+            serieNueva.ColumnaNombre = ColumnaNombre;
+            serieNueva.ColumnaValor = ColumnaValor;
+            ListaSeries.Add(serieNueva);
+            //Chart1.Series[serie].Points.DataBind(Datos.DefaultView, ColumnaNombre, ColumnaValor, "");
         }
 
         /// <summary>
@@ -144,15 +182,17 @@ namespace EDUAR_UI.UserControls
         /// </summary>
         public void GraficarBarra()
         {
-            Chart1.Series.Add("Default");
-            Chart1.Legends.Clear();
-            Chart1.Series["Default"]["DrawingStyle"] = "Emboss";
-            Chart1.Series["Default"].Color = System.Drawing.Color.Brown;
-            Chart1.Series["Default"].BackGradientStyle = GradientStyle.DiagonalLeft;
-            Chart1.Series["Default"].ShadowOffset = 2;
-            Chart1.Series["Default"].ToolTip = "#VALX : #VALY";
-           
-			Graficar();
+            foreach (MiSerie item in ListaSeries)
+            {
+                Chart1.Series.Add(item.NombreSerie);
+                Chart1.Series[item.NombreSerie]["DrawingStyle"] = "Emboss";
+                Chart1.Series[item.NombreSerie].Color = System.Drawing.Color.Blue;
+                Chart1.Series[item.NombreSerie].BackGradientStyle = GradientStyle.DiagonalLeft;
+                Chart1.Series[item.NombreSerie].ShadowOffset = 2;
+                Chart1.Series[item.NombreSerie].ToolTip = "#VALX : #VALY";
+            }
+
+            Graficar();
         }
 
         /// <summary>
@@ -160,14 +200,16 @@ namespace EDUAR_UI.UserControls
         /// </summary>
         public void GraficarLinea()
         {
-            Chart1.Series.Add("Default");
-            Chart1.Legends.Clear();
-            Chart1.Series["Default"].ChartType = SeriesChartType.Line;
-            Chart1.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = false;
-            Chart1.Series["Default"].Color = System.Drawing.Color.Brown;
-            Chart1.Series["Default"].BorderWidth = 4;
-            Chart1.Series["Default"].ShadowOffset = 2;
-
+            foreach (MiSerie item in ListaSeries)
+            {
+                Chart1.Series.Add(item.NombreSerie);
+                Chart1.Legends.Clear();
+                Chart1.Series[item.NombreSerie].ChartType = SeriesChartType.Line;
+                Chart1.ChartAreas[item.NombreSerie].Area3DStyle.Enable3D = false;
+                Chart1.Series[item.NombreSerie].Color = System.Drawing.Color.Blue;
+                Chart1.Series[item.NombreSerie].BorderWidth = 4;
+                Chart1.Series[item.NombreSerie].ShadowOffset = 2;
+            }
             Graficar();
         }
 
@@ -176,24 +218,27 @@ namespace EDUAR_UI.UserControls
         /// </summary>
         public void GraficarTorta3D()
         {
-			Chart1.Series.Add("Default");
-            Chart1.Series["Default"].ChartType = SeriesChartType.Pie;
-            Chart1.Series["Default"].ShadowOffset = 2;
-            Chart1.Series["Default"].ToolTip = "#VALX : #PERCENT";
-
-            if (LegendOutside)
+            foreach (MiSerie item in ListaSeries)
             {
-                Chart1.Series["Default"]["PieLabelStyle"] = "Outside";
-                Chart1.Series["Default"].Label = "#VALX";
+                Chart1.Series.Add(item.NombreSerie);
+                Chart1.Series[item.NombreSerie].ChartType = SeriesChartType.Pie;
+                Chart1.Series[item.NombreSerie].ShadowOffset = 2;
+                Chart1.Series[item.NombreSerie].ToolTip = "#VALX : #PERCENT";
+
+                if (LegendOutside)
+                {
+                    Chart1.Series[item.NombreSerie]["PieLabelStyle"] = "Outside";
+                    Chart1.Series[item.NombreSerie].Label = "#VALX";
+                }
+                else
+                    Chart1.Series[item.NombreSerie].Label = " ";
+
+                Graficar();
+
+                Chart1.Series[item.NombreSerie].LegendText = "#VALX : #PERCENT";
+                if (Chart1.Legends.Count > 0)
+                    Chart1.Legends[0].Alignment = StringAlignment.Center;
             }
-            else
-                Chart1.Series["Default"].Label = " ";
-
-            Graficar();
-
-            Chart1.Series["Default"].LegendText = "#VALX : #PERCENT";
-            if (Chart1.Legends.Count > 0)
-                Chart1.Legends[0].Alignment = StringAlignment.Center;
         }
 
         /// <summary>
@@ -201,25 +246,28 @@ namespace EDUAR_UI.UserControls
         /// </summary>
         public void GraficarRadar()
         {
-			Chart1.Series.Add("Default");
-            Chart1.Series["Default"].ChartType = SeriesChartType.Radar;
-            Chart1.Series["Default"].Color = System.Drawing.Color.Brown;
-            Chart1.Series["Default"].ToolTip = "#VALX : #PERCENT";
+            foreach (MiSerie item in ListaSeries)
+            {
+                Chart1.Series.Add(item.NombreSerie);
+                Chart1.Series[item.NombreSerie].ChartType = SeriesChartType.Radar;
+                Chart1.Series[item.NombreSerie].Color = System.Drawing.Color.Blue;
+                Chart1.Series[item.NombreSerie].ToolTip = "#VALX : #PERCENT";
 
-            Graficar();
+                Graficar();
 
-            // Set radar chart style (Area, Line or Marker)
-            Chart1.Series["Default"]["RadarDrawingStyle"] = "Area";
+                // Set radar chart style (Area, Line or Marker)
+                Chart1.Series[item.NombreSerie]["RadarDrawingStyle"] = "Area";
 
-            // Set circular area drawing style (Circle or Polygon)
-            Chart1.Series["Default"]["AreaDrawingStyle"] = "Polygon";
+                // Set circular area drawing style (Circle or Polygon)
+                Chart1.Series[item.NombreSerie]["AreaDrawingStyle"] = "Polygon";
 
-            // Set labels style (Auto, Horizontal, Circular or Radial)
-            Chart1.Series["Default"]["CircularLabelsStyle"] = "Horizontal";
+                // Set labels style (Auto, Horizontal, Circular or Radial)
+                Chart1.Series[item.NombreSerie]["CircularLabelsStyle"] = "Horizontal";
 
-            Chart1.Legends.Clear();
-            Chart1.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
-            Chart1.Legends.Clear();
+                Chart1.Legends.Clear();
+                Chart1.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+                Chart1.Legends.Clear();
+            }
         }
 
         /// <summary>
@@ -234,49 +282,55 @@ namespace EDUAR_UI.UserControls
         /// Carga los datos de un store procedure.
         /// </summary>
         /// <param name="NombreSP"></param>
-        public void CargarDatos<T>(List<T> lista)
+        //public void CargarDatos<T>(List<T> lista)
+        //{
+        //    Datos = UIUtilidades.BuildDataTable<T>(lista);
+        //}
+
+        public void LimpiarSeries()
         {
-			Datos = UIUtilidades.BuildDataTable<T>(lista);
+            Chart1.Series.Clear();
+            ListaSeries.Clear();
         }
 
         protected void btnLineal_Click(object sender, EventArgs e)
-		{
-			divGrafico.Attributes.Add("class", "divGraficoOcultar");
-			TDGrafico.Attributes.Add("class", "divGraficoOcultar");
-			Chart1.Series.Clear();
+        {
+            divGrafico.Attributes.Add("class", "divGraficoOcultar");
+            TDGrafico.Attributes.Add("class", "divGraficoOcultar");
+            Chart1.Series.Clear();
             GraficarLinea();
         }
 
         protected void btnBar_Click(object sender, EventArgs e)
         {
-			divGrafico.Attributes.Add("class", "divGraficoOcultar");
-			TDGrafico.Attributes.Add("class", "divGraficoOcultar");
-			Chart1.Series.Clear();
+            divGrafico.Attributes.Add("class", "divGraficoOcultar");
+            TDGrafico.Attributes.Add("class", "divGraficoOcultar");
+            Chart1.Series.Clear();
             GraficarBarra();
         }
 
         protected void btnTorta_Click(object sender, EventArgs e)
         {
-			divGrafico.Attributes.Add("class", "divGraficoOcultar");
-			TDGrafico.Attributes.Add("class", "divGraficoOcultar");
-			Chart1.Series.Clear();
+            divGrafico.Attributes.Add("class", "divGraficoOcultar");
+            TDGrafico.Attributes.Add("class", "divGraficoOcultar");
+            Chart1.Series.Clear();
             GraficarTorta3D();
         }
 
         protected void btnRadar_Click(object sender, EventArgs e)
         {
-			divGrafico.Attributes.Add("class", "divGraficoOcultar");
-			TDGrafico.Attributes.Add("class", "divGraficoOcultar");
-			Chart1.Series.Clear();
+            divGrafico.Attributes.Add("class", "divGraficoOcultar");
+            TDGrafico.Attributes.Add("class", "divGraficoOcultar");
+            Chart1.Series.Clear();
             GraficarRadar();
         }
 
-		protected void btnCerrar_Click(object sender, EventArgs e)
-		{
-			divGrafico.Attributes.Add("class", "divGraficoOcultar");
-			TDGrafico.Attributes.Add("class", "divGraficoOcultar");
-			TDBotonera.Attributes.Add("class", "BotoneraGraficoOcultar");
-			Chart1.Series.Clear();
-		}
+        protected void btnCerrar_Click(object sender, EventArgs e)
+        {
+            divGrafico.Attributes.Add("class", "divGraficoOcultar");
+            TDGrafico.Attributes.Add("class", "divGraficoOcultar");
+            TDBotonera.Attributes.Add("class", "BotoneraGraficoOcultar");
+            Chart1.Series.Clear();
+        }
     }
 }
