7fc7ae88c0a16d2b1506c81acb4b1162951983ae
martinh3 <martinh3@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2011-08-30 04:20:35 +0000

    Se hizo NO Nullable fechaNacimiento para User Stories de Sprint 2. Verificar que no rompa nada

--- Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_DataTransferObject/Entities/Package Usuarios/Persona.cs
+++ Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_DataTransferObject/Entities/Package Usuarios/Persona.cs
@@ -15,12 +15,13 @@ namespace EDUAR_Entities
         public string barrio { get; set; }
         public int idLocalidad { get; set; }
         public string sexo { get; set; }
-        public DateTime? fechaNacimiento { get; set; }
+        // Verificar que al hacer fechaNacimiento No Nullable no rompa nada
+		public DateTime fechaNacimiento { get; set; }
         public string telefonoFijo { get; set; }
         public string telefonoCelular { get; set; }
         public string telefonoCelularAlternativo { get; set; }
         public string email { get; set; }
-        public bool? activo { get; set; }
+ 		public bool? activo { get; set; }
         public Localidades localidad { get; set; }
         public string username { get; set; }
         public int idTipoPersona { get; set; }
