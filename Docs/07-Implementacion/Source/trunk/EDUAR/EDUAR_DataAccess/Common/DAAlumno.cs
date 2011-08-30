ec139fb8e527a6cbaa921a92f3d5a7a64512adb1
martinh3 <martinh3@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2011-08-30 04:40:34 +0000

    User Story - Sprint 2

--- Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_DataAccess/Common/DAAlumno.cs
+++ Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_DataAccess/Common/DAAlumno.cs
@@ -1,6 +1,16 @@
 ﻿using System;
 using EDUAR_DataAccess.Shared;
 using EDUAR_Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EDUAR_Entities;
+using EDUAR_DataAccess.Shared;
+using EDUAR_Utility.Enumeraciones;
+using EDUAR_Utility.Excepciones;
+using System.Data.SqlClient;
+using System.Data;
+using EDUAR_Utility.Constantes;
 
 namespace EDUAR_DataAccess.Common
 {
@@ -60,6 +70,116 @@ namespace EDUAR_DataAccess.Common
         #endregion
 
         #region --[Métodos Públicos]--
+
+        //public List<Alumno> GetAlumnos(Alumno entidad)
+        //{
+        //    try
+        //    {
+        //        Transaction.DBcomand = Transaction.DataBase.GetStoredProcCommand("Alumno_Select");
+        //        if (entidad != null)
+        //        {
+        //            if (entidad.idAlumno > 0)
+        //                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idAlumno", DbType.Int32, entidad.idAlumno);
+        //            if (entidad.activo != null)
+        //                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@activo", DbType.Boolean, entidad.activo);
+        //            if (!string.IsNullOrEmpty(entidad.apellido))
+        //                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@apellido", DbType.String, entidad.apellido);
+        //            if (!string.IsNullOrEmpty(entidad.barrio))
+        //                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "barrio", DbType.String, entidad.barrio);
+        //            if (!string.IsNullOrEmpty(entidad.domicilio))
+        //                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@domicilio", DbType.String, entidad.domicilio);
+        //            if (!string.IsNullOrEmpty(entidad.email))
+        //                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@email", DbType.String, entidad.email);
+        //            if (ValidarFechaSQL(entidad.fechaAlta))
+        //                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@fechaAlta", DbType.Date, entidad.fechaAlta);
+        //            if (ValidarFechaSQL(entidad.fechaBaja))
+        //                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@fechaBaja", DbType.Date, entidad.fechaBaja);
+        //            if (entidad.fechaNacimiento != null && ValidarFechaSQL((DateTime)entidad.fechaNacimiento))
+        //                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@fechaNacimiento", DbType.Date, entidad.fechaNacimiento);
+        //        }
+            
+        //        IDataReader reader = Transaction.DataBase.ExecuteReader(Transaction.DBcomand);
+
+        //        List<Alumno> listaAlumnos = new List<Alumno>();
+        //        Alumno objAlumno;
+        //        while (reader.Read())
+        //        {
+        //            objAlumno = new Alumno();
+
+        //            objAlumno.idAlumno = Convert.ToInt32(reader["idAlumno"]);
+        //            objAlumno.apellido = reader["apellido"].ToString();
+        //            if (!string.IsNullOrEmpty(reader["fechaAlta"].ToString()))
+        //                objAlumno.fechaAlta = (DateTime)reader["fechaAlta"];
+        //            if (!string.IsNullOrEmpty(reader["fechaBaja"].ToString()))
+        //                objAlumno.fechaBaja = (DateTime)reader["fechaBaja"];
+        //            objAlumno.activo = Convert.ToBoolean(reader["activo"]);
+                   
+        //             //TODO: Completar los miembros que faltan de alumno
+                        
+        //             listaAlumnos.Add(objAlumno);
+        //        }
+        //        return listaAlumnos;
+        //    }
+        //    catch (SqlException ex)
+        //    {
+        //        throw new CustomizedException(string.Format("Fallo en {0} - GetCiclosLectivos()", ClassName),
+        //                            ex, enuExceptionType.SqlException);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new CustomizedException(string.Format("Fallo en {0} - GetCiclosLectivos()", ClassName),
+        //                            ex, enuExceptionType.DataAccesException);
+        //    }
+        //}
+
+        public List<Alumno> GetAlumnos(AlumnoCurso entidad)
+        {
+            try
+            {
+                Transaction.DBcomand = Transaction.DataBase.GetStoredProcCommand("AlumnosPorCurso_Select");
+                //'AlumnoPorCurso_Select
+                if (entidad != null)
+                {
+                    if (entidad.alumno.idAlumno > 0)
+                        Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idAlumno", DbType.Int32, entidad.alumno.idAlumno);
+                    if (entidad.curso.idCurso > 0)
+                        Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idCurso", DbType.Int32, entidad.curso.idCurso);
+                  
+               }
+
+                IDataReader reader = Transaction.DataBase.ExecuteReader(Transaction.DBcomand);
+
+                List<Alumno> listaAlumnos = new List<Alumno>();
+                Alumno objAlumno;
+                while (reader.Read())
+                {
+                    objAlumno = new Alumno();
+
+                    objAlumno.idAlumno = Convert.ToInt32(reader["idAlumno"]);
+                    objAlumno.apellido = reader["apellido"].ToString();
+                    if (!string.IsNullOrEmpty(reader["fechaAlta"].ToString()))
+                        objAlumno.fechaAlta = (DateTime)reader["fechaAlta"];
+                    if (!string.IsNullOrEmpty(reader["fechaBaja"].ToString()))
+                        objAlumno.fechaBaja = (DateTime)reader["fechaBaja"];
+                    //objAlumno.activo = Convert.ToBoolean(reader["activo"]);
+
+                    //TODO: Completar los miembros que faltan de alumno
+
+                    listaAlumnos.Add(objAlumno);
+                }
+                return listaAlumnos;
+            }
+            catch (SqlException ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - GetAlumnosPorCurso()", ClassName),
+                                    ex, enuExceptionType.SqlException);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - GetalumnosPorCurso()", ClassName),
+                                    ex, enuExceptionType.DataAccesException);
+            }
+        }
         #endregion
     }
 }
