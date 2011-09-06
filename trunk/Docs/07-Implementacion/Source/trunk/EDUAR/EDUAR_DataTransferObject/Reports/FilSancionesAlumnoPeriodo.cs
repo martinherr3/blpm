fb9075373e7823ad88c7aac79589fa1c19b81255
king.orco <king.orco@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2011-09-06 22:36:02 +0000

    Reportes de inasistencias y sanciones

---
+++ trunk/Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_DataTransferObject/Reports/FilSancionesAlumnoPeriodo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EDUAR_Entities.Shared;
+
+namespace EDUAR_Entities.Reports
+{
+    [Serializable]
+    public class FilSancionesAlumnoPeriodo : DTBase
+    {
+        public int idAlumno { get; set; }
+        public int idCurso { get; set; }
+		public int idCicloLectivo { get; set; }
+        public DateTime fechaDesde { get; set; }
+        public DateTime fechaHasta { get; set; }
+
+        public FilSancionesAlumnoPeriodo()
+        {
+        }
+    }
+}
