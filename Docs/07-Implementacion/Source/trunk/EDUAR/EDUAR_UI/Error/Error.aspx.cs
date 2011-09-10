306d8f42b896d8d777d12f73b497a29eb99cc470
laupastorino <laupastorino@gmail.com>
2011-09-10 22:12:05 +0000

    Lau - retoques visuales

---
+++ Docs/07-Implementacion/Source/trunk/EDUAR/EDUAR_UI/Error/Error.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace EDUAR_UI.Error
+{
+	public partial class Error : Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (Application["CurrentError"] != null && HttpContext.Current.IsDebuggingEnabled)
+			{
+				divDetalle.Visible = true;
+				lblError.Text = (string)Application["CurrentError"];
+				lblDetalle.Text = (string)Application["CurrentErrorDetalle"];
+				Application["CurrentError"] = null;
+				Application["CurrentErrorDetalle"] = null;
+			}
+			else
+				divDetalle.Visible = false;
+		}
+	}
+}
