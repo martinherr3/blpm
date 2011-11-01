034b0e2aaf678a1e0be36ef3fae60269952ea327
laupastorino@gmail.com <laupastorino@gmail.com@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2011-11-01 04:23:07 +0000

    Lau - correcciones en sanciones e inasistencias en boletín

--- trunk/Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_BusinessLogic/Reports/BLRptInasistenciasAlumnoPeriodo.cs
+++ trunk/Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_BusinessLogic/Reports/BLRptInasistenciasAlumnoPeriodo.cs
@@ -10,109 +10,136 @@ using EDUAR_Utility.Excepciones;
 
 namespace EDUAR_BusinessLogic.Reports
 {
-    public class BLRptInasistenciasAlumnoPeriodo : BusinessLogicBase<RptInasistenciasAlumnoPeriodo, DARptInasistenciasAlumnoPeriodo>
-    {
-        #region --[Constante]--
-        private const string ClassName = "BLRptInasistenciasAlumnoPeriodo";
-        #endregion
+	public class BLRptInasistenciasAlumnoPeriodo : BusinessLogicBase<RptInasistenciasAlumnoPeriodo, DARptInasistenciasAlumnoPeriodo>
+	{
+		#region --[Constante]--
+		private const string ClassName = "BLRptInasistenciasAlumnoPeriodo";
+		#endregion
 
-        #region --[Constructores]--
-        /// <summary>
-        /// Constructor con DTO como parámetro.
-        /// </summary>
-        public BLRptInasistenciasAlumnoPeriodo(DTBase objRptInasistenciasAlumnoPeriodo)
-        {
-            Data = (RptInasistenciasAlumnoPeriodo)objRptInasistenciasAlumnoPeriodo;
-        }
-        /// <summary>
-        /// Constructor vacio
-        /// </summary>
-        public BLRptInasistenciasAlumnoPeriodo()
-        {
-            Data = new RptInasistenciasAlumnoPeriodo();
-        }
-        #endregion
+		#region --[Constructores]--
+		/// <summary>
+		/// Constructor con DTO como parámetro.
+		/// </summary>
+		public BLRptInasistenciasAlumnoPeriodo(DTBase objRptInasistenciasAlumnoPeriodo)
+		{
+			Data = (RptInasistenciasAlumnoPeriodo)objRptInasistenciasAlumnoPeriodo;
+		}
+		/// <summary>
+		/// Constructor vacio
+		/// </summary>
+		public BLRptInasistenciasAlumnoPeriodo()
+		{
+			Data = new RptInasistenciasAlumnoPeriodo();
+		}
+		#endregion
 
-        #region --[Propiedades Override]--
-        protected override sealed DARptInasistenciasAlumnoPeriodo DataAcces
-        {
-            get { return dataAcces; }
-            set { dataAcces = value; }
-        }
+		#region --[Propiedades Override]--
+		protected override sealed DARptInasistenciasAlumnoPeriodo DataAcces
+		{
+			get { return dataAcces; }
+			set { dataAcces = value; }
+		}
 
-        public override sealed RptInasistenciasAlumnoPeriodo Data
-        {
-            get { return data; }
-            set { data = value; }
-        }
+		public override sealed RptInasistenciasAlumnoPeriodo Data
+		{
+			get { return data; }
+			set { data = value; }
+		}
 
-        public override string FieldId
-        {
-            get { return DataAcces.FieldID; }
-        }
+		public override string FieldId
+		{
+			get { return DataAcces.FieldID; }
+		}
 
-        public override string FieldDescription
-        {
-            get { return DataAcces.FieldDescription; }
-        }
+		public override string FieldDescription
+		{
+			get { return DataAcces.FieldDescription; }
+		}
 
-        /// <summary>
-        /// Gets the by id.
-        /// </summary>
-        public override void GetById()
-        {
-            throw new NotImplementedException();
-        }
+		/// <summary>
+		/// Gets the by id.
+		/// </summary>
+		public override void GetById()
+		{
+			throw new NotImplementedException();
+		}
 
-        /// <summary>
-        /// Método que guarda el registro actualmente cargado en memoria. No importa si se trata de una alta o modificación.
-        /// </summary>
-        public override void Save()
-        {
-            throw new NotImplementedException();
-        }
+		/// <summary>
+		/// Método que guarda el registro actualmente cargado en memoria. No importa si se trata de una alta o modificación.
+		/// </summary>
+		public override void Save()
+		{
+			throw new NotImplementedException();
+		}
 
-        /// <summary>
-        /// Método que guarda el registro actualmente cargado en memoria. No importa si se trata de una alta o modificación.
-        /// </summary>
-        public override void Save(DATransaction objDATransaction)
-        {
-            throw new NotImplementedException();
-        }
+		/// <summary>
+		/// Método que guarda el registro actualmente cargado en memoria. No importa si se trata de una alta o modificación.
+		/// </summary>
+		public override void Save(DATransaction objDATransaction)
+		{
+			throw new NotImplementedException();
+		}
 
-        public override void Delete()
-        {
-            throw new NotImplementedException();
-        }
+		public override void Delete()
+		{
+			throw new NotImplementedException();
+		}
 
-        public override void Delete(DATransaction objDATransaction)
-        {
-            throw new NotImplementedException();
-        }
-        #endregion
+		public override void Delete(DATransaction objDATransaction)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
 
-        #region --[Métodos publicos]--
+		#region --[Métodos publicos]--
 		/// <summary>
 		/// Gets the RPT inasistencias alumno periodo.
 		/// </summary>
 		/// <param name="entidad">The entidad.</param>
 		/// <returns></returns>
-        public List<RptInasistenciasAlumnoPeriodo> GetRptInasistenciasAlumnoPeriodo(FilInasistenciasAlumnoPeriodo entidad)
-        {
-            try
-            {
-                return DataAcces.GetRptInasistenciasAlumnoPeriodo(entidad);
-            }
-            catch (CustomizedException ex)
-            {
-                throw ex;
-            }
-            catch (Exception ex)
-            {
-                throw new CustomizedException(string.Format("Fallo en {0} - GetRptInasistenciasAlumnoPeriodo", ClassName), ex,
-                                              enuExceptionType.BusinessLogicException);
-            }
-        }
-        #endregion
-    }
+		public List<RptInasistenciasAlumnoPeriodo> GetRptInasistenciasAlumnoPeriodo(FilInasistenciasAlumnoPeriodo entidad)
+		{
+			try
+			{
+				return DataAcces.GetRptInasistenciasAlumnoPeriodo(entidad);
+			}
+			catch (CustomizedException ex)
+			{
+				throw ex;
+			}
+			catch (Exception ex)
+			{
+				throw new CustomizedException(string.Format("Fallo en {0} - GetRptInasistenciasAlumnoPeriodo", ClassName), ex,
+											  enuExceptionType.BusinessLogicException);
+			}
+		}
+
+		/// <summary>
+		/// Gets the RPT inasistencias alumno periodo.
+		/// </summary>
+		/// <param name="entidad">The entidad.</param>
+		/// <returns></returns>
+		public List<RptInasistenciasAlumnoPeriodo> GetRptInasistenciasAlumnoPeriodo(FilIncidenciasAlumno entidad)
+		{
+			try
+			{
+				FilInasistenciasAlumnoPeriodo filtro = new FilInasistenciasAlumnoPeriodo();
+				filtro.idAlumno = entidad.idAlumno;
+				filtro.idCicloLectivo = entidad.idCicloLectivo;
+				filtro.idCurso = entidad.idCurso;
+
+				return DataAcces.GetRptInasistenciasAlumnoPeriodo(filtro);
+			}
+			catch (CustomizedException ex)
+			{
+				throw ex;
+			}
+			catch (Exception ex)
+			{
+				throw new CustomizedException(string.Format("Fallo en {0} - GetRptInasistenciasAlumnoPeriodo", ClassName), ex,
+											  enuExceptionType.BusinessLogicException);
+			}
+		}
+		#endregion
+	}
 }
--- trunk/Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_BusinessLogic/Reports/BLRptSancionesAlumnoPeriodo.cs
+++ trunk/Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_BusinessLogic/Reports/BLRptSancionesAlumnoPeriodo.cs
@@ -10,109 +10,136 @@ using EDUAR_Utility.Excepciones;
 
 namespace EDUAR_BusinessLogic.Reports
 {
-    public class BLRptSancionesAlumnoPeriodo : BusinessLogicBase<RptSancionesAlumnoPeriodo, DARptSancionesAlumnoPeriodo>
-    {
-        #region --[Constante]--
-        private const string ClassName = "BLRptSancionesAlumnoPeriodo";
-        #endregion
+	public class BLRptSancionesAlumnoPeriodo : BusinessLogicBase<RptSancionesAlumnoPeriodo, DARptSancionesAlumnoPeriodo>
+	{
+		#region --[Constante]--
+		private const string ClassName = "BLRptSancionesAlumnoPeriodo";
+		#endregion
 
-        #region --[Constructores]--
-        /// <summary>
-        /// Constructor con DTO como parámetro.
-        /// </summary>
-        public BLRptSancionesAlumnoPeriodo(DTBase objRptSancionesAlumnoPeriodo)
-        {
-            Data = (RptSancionesAlumnoPeriodo)objRptSancionesAlumnoPeriodo;
-        }
-        /// <summary>
-        /// Constructor vacio
-        /// </summary>
-        public BLRptSancionesAlumnoPeriodo()
-        {
-            Data = new RptSancionesAlumnoPeriodo();
-        }
-        #endregion
+		#region --[Constructores]--
+		/// <summary>
+		/// Constructor con DTO como parámetro.
+		/// </summary>
+		public BLRptSancionesAlumnoPeriodo(DTBase objRptSancionesAlumnoPeriodo)
+		{
+			Data = (RptSancionesAlumnoPeriodo)objRptSancionesAlumnoPeriodo;
+		}
+		/// <summary>
+		/// Constructor vacio
+		/// </summary>
+		public BLRptSancionesAlumnoPeriodo()
+		{
+			Data = new RptSancionesAlumnoPeriodo();
+		}
+		#endregion
 
-        #region --[Propiedades Override]--
-        protected override sealed DARptSancionesAlumnoPeriodo DataAcces
-        {
-            get { return dataAcces; }
-            set { dataAcces = value; }
-        }
+		#region --[Propiedades Override]--
+		protected override sealed DARptSancionesAlumnoPeriodo DataAcces
+		{
+			get { return dataAcces; }
+			set { dataAcces = value; }
+		}
 
-        public override sealed RptSancionesAlumnoPeriodo Data
-        {
-            get { return data; }
-            set { data = value; }
-        }
+		public override sealed RptSancionesAlumnoPeriodo Data
+		{
+			get { return data; }
+			set { data = value; }
+		}
 
-        public override string FieldId
-        {
-            get { return DataAcces.FieldID; }
-        }
+		public override string FieldId
+		{
+			get { return DataAcces.FieldID; }
+		}
 
-        public override string FieldDescription
-        {
-            get { return DataAcces.FieldDescription; }
-        }
+		public override string FieldDescription
+		{
+			get { return DataAcces.FieldDescription; }
+		}
 
-        /// <summary>
-        /// Gets the by id.
-        /// </summary>
-        public override void GetById()
-        {
-            throw new NotImplementedException();
-        }
+		/// <summary>
+		/// Gets the by id.
+		/// </summary>
+		public override void GetById()
+		{
+			throw new NotImplementedException();
+		}
 
-        /// <summary>
-        /// Método que guarda el registro actualmente cargado en memoria. No importa si se trata de una alta o modificación.
-        /// </summary>
-        public override void Save()
-        {
-            throw new NotImplementedException();
-        }
+		/// <summary>
+		/// Método que guarda el registro actualmente cargado en memoria. No importa si se trata de una alta o modificación.
+		/// </summary>
+		public override void Save()
+		{
+			throw new NotImplementedException();
+		}
 
-        /// <summary>
-        /// Método que guarda el registro actualmente cargado en memoria. No importa si se trata de una alta o modificación.
-        /// </summary>
-        public override void Save(DATransaction objDATransaction)
-        {
-            throw new NotImplementedException();
-        }
+		/// <summary>
+		/// Método que guarda el registro actualmente cargado en memoria. No importa si se trata de una alta o modificación.
+		/// </summary>
+		public override void Save(DATransaction objDATransaction)
+		{
+			throw new NotImplementedException();
+		}
 
-        public override void Delete()
-        {
-            throw new NotImplementedException();
-        }
+		public override void Delete()
+		{
+			throw new NotImplementedException();
+		}
 
-        public override void Delete(DATransaction objDATransaction)
-        {
-            throw new NotImplementedException();
-        }
-        #endregion
+		public override void Delete(DATransaction objDATransaction)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
 
-        #region --[Métodos publicos]--
+		#region --[Métodos publicos]--
 		/// <summary>
 		/// Gets the RPT sanciones alumno periodo.
 		/// </summary>
 		/// <param name="entidad">The entidad.</param>
 		/// <returns></returns>
-        public List<RptSancionesAlumnoPeriodo> GetRptSancionesAlumnoPeriodo(FilSancionesAlumnoPeriodo entidad)
-        {
-            try
-            {
-                return DataAcces.GetRptSancionesAlumnoPeriodo(entidad);
-            }
-            catch (CustomizedException ex)
-            {
-                throw ex;
-            }
-            catch (Exception ex)
-            {
-                throw new CustomizedException(string.Format("Fallo en {0} - GetRptSancionesAlumnoPeriodo", ClassName), ex,
-                                              enuExceptionType.BusinessLogicException);
-            }
-        }
-        #endregion
-    }
+		public List<RptSancionesAlumnoPeriodo> GetRptSancionesAlumnoPeriodo(FilSancionesAlumnoPeriodo entidad)
+		{
+			try
+			{
+				return DataAcces.GetRptSancionesAlumnoPeriodo(entidad);
+			}
+			catch (CustomizedException ex)
+			{
+				throw ex;
+			}
+			catch (Exception ex)
+			{
+				throw new CustomizedException(string.Format("Fallo en {0} - GetRptSancionesAlumnoPeriodo", ClassName), ex,
+											  enuExceptionType.BusinessLogicException);
+			}
+		}
+
+		/// <summary>
+		/// Gets the RPT sanciones alumno periodo.
+		/// </summary>
+		/// <param name="entidad">The entidad.</param>
+		/// <returns></returns>
+		public List<RptSancionesAlumnoPeriodo> GetRptSancionesAlumnoPeriodo(FilIncidenciasAlumno entidad)
+		{
+			try
+			{
+				FilSancionesAlumnoPeriodo filtro = new FilSancionesAlumnoPeriodo();
+				filtro.idAlumno = entidad.idAlumno;
+				filtro.idCicloLectivo = entidad.idCicloLectivo;
+				filtro.idCurso = entidad.idCurso;
+
+				return DataAcces.GetRptSancionesAlumnoPeriodo(filtro);
+			}
+			catch (CustomizedException ex)
+			{
+				throw ex;
+			}
+			catch (Exception ex)
+			{
+				throw new CustomizedException(string.Format("Fallo en {0} - GetRptSancionesAlumnoPeriodo", ClassName), ex,
+											  enuExceptionType.BusinessLogicException);
+			}
+		}
+		#endregion
+	}
 }
