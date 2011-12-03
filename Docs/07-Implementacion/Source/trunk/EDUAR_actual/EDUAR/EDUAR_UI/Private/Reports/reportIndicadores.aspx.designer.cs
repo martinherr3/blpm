6c2b8f61d0a051b03f7c749cb06aa20a19ffc9d1
laupastorino <laupastorino@gmail.com>
2011-12-03 23:50:11 +0000

    Lau - Reporte Indicadores (DEC - Promethee)

--- Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_UI/Private/Reports/reportIndicadores.aspx.designer.cs
+++ Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_UI/Private/Reports/reportIndicadores.aspx.designer.cs
@@ -130,6 +130,15 @@ namespace EDUAR_UI {
         protected global::System.Web.UI.UpdatePanel udpResultado;
         
         /// <summary>
+        /// Control lblResultadoGrilla.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblResultadoGrilla;
+        
+        /// <summary>
         /// Control lblResultado.
         /// </summary>
         /// <remarks>
