c75f9575eeedd2492b2136cb58e8dd6f4ecf5e5f
laupastorino <laupastorino@gmail.com>
2012-04-17 00:06:37 +0000

    Lau - Issue 3: Modificar Usuario

--- Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_UI/Private/Account/ChangeUser.aspx.designer.cs
+++ Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_UI/Private/Account/ChangeUser.aspx.designer.cs
@@ -67,13 +67,13 @@ namespace EDUAR_UI {
         protected global::System.Web.UI.UpdatePanel udpFiltrosBusqueda;
         
         /// <summary>
-        /// Control lblUserNameBusqeuda.
+        /// Control lblUserNameBusqueda.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblUserNameBusqeuda;
+        protected global::System.Web.UI.WebControls.Label lblUserNameBusqueda;
         
         /// <summary>
         /// Control txtUsernameBusqueda.
