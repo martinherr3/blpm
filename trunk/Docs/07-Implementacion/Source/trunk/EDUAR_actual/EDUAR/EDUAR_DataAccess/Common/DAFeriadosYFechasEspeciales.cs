2a0f55de5b29e8969fb3cefd68c2a178836a5f5d
martinh3 <martinh3@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2012-04-22 16:45:50 +0000

    Arreglo de  Issue 5:	Horarios para citas

--- trunk/Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_DataAccess/Common/DAFeriadosYFechasEspeciales.cs
+++ trunk/Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_DataAccess/Common/DAFeriadosYFechasEspeciales.cs
@@ -100,6 +100,87 @@ namespace EDUAR_DataAccess.Common
 									ex, enuExceptionType.DataAccesException);
 			}
 		}
+
+        public DateTime getHorarioInicio()
+        {
+            DateTime retHorario = new DateTime();
+                
+            string HorarioInicio = "";
+            string []horaYMinutosInicio;
+
+            try
+            {
+                Transaction.DBcomand = Transaction.DataBase.GetStoredProcCommand("ConfiguracionesEstablecimiento_Select");
+      
+                // Agregar como parametro de la consulta el id donde se que esta guardado el horario de inicio
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idConfiguracionEstablecimiento", DbType.Int32, 3);
+      
+
+                IDataReader reader = Transaction.DataBase.ExecuteReader(Transaction.DBcomand);
+
+                while (reader.Read())
+                {
+                    HorarioInicio = reader["valor"].ToString();
+                }
+
+                horaYMinutosInicio = HorarioInicio.Split(':');
+                retHorario = new DateTime(System.DateTime.Now.Year, System.DateTime.Now.Month, System.DateTime.Now.Day, int.Parse(horaYMinutosInicio[0]), int.Parse(horaYMinutosInicio[1]), 0);     
+            }
+            catch (SqlException ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - getHorarioInicio()", ClassName),
+                                    ex, enuExceptionType.SqlException);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - getHorarioInicio()", ClassName),
+                                    ex, enuExceptionType.DataAccesException);
+            }
+            ////////////////////////
+            return (retHorario);
+        }
+
+        public DateTime getHorarioFinalizacion()
+        {
+            DateTime retHorario = new DateTime();
+            string HorarioFinalizacion = "";
+            string[] horaYMinutosFinalizacion;
+
+            try
+            {
+                Transaction.DBcomand = Transaction.DataBase.GetStoredProcCommand("ConfiguracionesEstablecimiento_Select");
+
+                // Agregar como parametro de la consulta el id donde se que esta guardado el horario de finalizacion
+                Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idConfiguracionEstablecimiento", DbType.Int32, 4);
+
+
+                IDataReader reader = Transaction.DataBase.ExecuteReader(Transaction.DBcomand);
+
+                while (reader.Read())
+                {
+                    HorarioFinalizacion = reader["valor"].ToString();
+                }
+                horaYMinutosFinalizacion = HorarioFinalizacion.Split(':');
+                retHorario = new DateTime(System.DateTime.Now.Year, System.DateTime.Now.Month, System.DateTime.Now.Day, int.Parse(horaYMinutosFinalizacion[0]), int.Parse(horaYMinutosFinalizacion[1]), 0);     
+
+
+
+            }
+            catch (SqlException ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - getHorarioFinalizacion()", ClassName),
+                                    ex, enuExceptionType.SqlException);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - getHorarioFinalizacion()", ClassName),
+                                    ex, enuExceptionType.DataAccesException);
+            }
+            ////////////////////////
+
+
+            return (retHorario);
+        }
 		#endregion
 	}
 }
