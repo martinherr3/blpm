6ec2eb0ca5c8071bd8a225a95d0b0d5349a8685a
martinh3 <martinh3@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2012-10-23 03:51:34 +0000

    Implementar funcionalidad SMS

--- Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_Utility/Utilidades/EDUARSMS.cs
+++ Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_Utility/Utilidades/EDUARSMS.cs
@@ -6,6 +6,7 @@ using EDUAR_Utility.Enumeraciones;
 using EDUAR_Utility.Excepciones;
 using SMSapplication;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace EDUAR_Utility.Utilidades
 {
@@ -69,6 +70,21 @@ namespace EDUAR_Utility.Utilidades
                 throw new CustomizedException(string.Format("Fallo en {0} - EnviarSMS()", ClassName),
                                                         ex, enuExceptionType.Exception);
             }
+        }
+
+        public string RemoveAccentsWithRegEx(string inputString)
+        {
+            Regex replace_a_Accents = new Regex("[á|à|ä|â]", RegexOptions.Compiled);
+            Regex replace_e_Accents = new Regex("[é|è|ë|ê]", RegexOptions.Compiled);
+            Regex replace_i_Accents = new Regex("[í|ì|ï|î]", RegexOptions.Compiled);
+            Regex replace_o_Accents = new Regex("[ó|ò|ö|ô]", RegexOptions.Compiled);
+            Regex replace_u_Accents = new Regex("[ú|ù|ü|û]", RegexOptions.Compiled);
+            inputString = replace_a_Accents.Replace(inputString, "a");
+            inputString = replace_e_Accents.Replace(inputString, "e");
+            inputString = replace_i_Accents.Replace(inputString, "i");
+            inputString = replace_o_Accents.Replace(inputString, "o");
+            inputString = replace_u_Accents.Replace(inputString, "u");
+            return inputString;
         }
         #endregion
 
