3c1a3c50c6564aa9a6a2f7356ff0c4441fcb6eac
laupastorino <laupastorino@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2013-04-09 15:58:56 +0000

    Lau - Indicadores Generales

---
+++ trunk/Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_UI/Private/Monitoreo/IndicadoresGenerales.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Title="Indicadores Generales" Language="C#" MasterPageFile="~/EDUARMaster.Master"
+    AutoEventWireup="true" CodeBehind="IndicadoresGenerales.aspx.cs" Inherits="EDUAR_UI.IndicadoresGenerales" %>
+
+<%@ MasterType VirtualPath="~/EDUARMaster.Master" %>
+<%@ Register Src="~/UserControls/Indicador.ascx" TagName="Indicador" TagPrefix="uc1" %>
+<%@ Register Src="~/UserControls/InformeIndicador.ascx" TagName="InformeIndicador"
+    TagPrefix="uc1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <table class="tablaInterna" cellpadding="0" cellspacing="0">
+        <tr>
+            <td class="TD90">
+                <h2>
+                    <asp:UpdatePanel runat="server">
+                        <ContentTemplate>
+                            <asp:Label ID="lblTitulo" Text="Indicadores De Desempeño" runat="server" /> - 
+                            <asp:Label ID="lblIndicador" Text="" runat="server" />
+                        </ContentTemplate>
+                        <Triggers>
+                            <asp:AsyncPostBackTrigger ControlID="dtlIndicadores" EventName="ItemCommand" />
+                        </Triggers>
+                    </asp:UpdatePanel>
+                </h2>
+                <br />
+            </td>
+            <td align="right" rowspan="2">
+                <%--<asp:ImageButton ID="btnVolverAnterior" OnClick="btnVolverAnterior_Click" runat="server"
+                    ToolTip="Volver" ImageUrl="~/Images/botonVolver.png" Visible="true" />--%>
+            </td>
+        </tr>
+    </table>
+    <table class="tablaInterna" cellpadding="1" cellspacing="5">
+        <tr>
+            <td>
+            </td>
+        </tr>
+    </table>
+    <asp:UpdatePanel ID="udpIndicadores" runat="server" UpdateMode="Conditional">
+        <ContentTemplate>
+            <asp:DataList RepeatLayout="Table" RepeatColumns="6" RepeatDirection="Horizontal"
+                runat="server" ID="dtlIndicadores" Width="100%" ShowHeader="true" ItemStyle-HorizontalAlign="Center"
+                OnItemCommand="dtlIndicadores_ItemCommand">
+                <ItemTemplate>
+                    <asp:LinkButton ID="indicador" CommandArgument='<%# Bind("idIndicador") %>' CommandName="Indicador"
+                        Text='<%# Bind("nombre") %>' runat="server" />
+                </ItemTemplate>
+            </asp:DataList>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+    <asp:UpdatePanel ID="udpCursos" runat="server" UpdateMode="Conditional">
+        <ContentTemplate>
+            <div id="divCursos" runat="server">
+                <asp:DataList RepeatLayout="Table" RepeatColumns="4" RepeatDirection="Horizontal"
+                    runat="server" ID="dtlCursos" Width="100%" ShowHeader="true" ItemStyle-HorizontalAlign="Center">
+                    <ItemTemplate>
+                        <uc1:Indicador ID="indicador" runat="server" idCursoIndicador='<%# Bind("idCurso") %>'
+                            cursoIndicador='<%# Bind("nombre") %>'></uc1:Indicador>
+                    </ItemTemplate>
+                    <AlternatingItemTemplate>
+                        <uc1:Indicador ID="indicador2" runat="server" idCursoIndicador='<%# Bind("idCurso") %>'
+                            cursoIndicador='<%# Bind("nombre") %>'></uc1:Indicador>
+                    </AlternatingItemTemplate>
+                </asp:DataList>
+            </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+    <asp:HiddenField ID="HiddenField1" runat="server" />
+    <ajaxToolkit:ModalPopupExtender ID="mpeContenido" runat="server" PopupControlID="pnlContenidos"
+        TargetControlID="HiddenField1" RepositionMode="RepositionOnWindowResizeAndScroll"
+        BackgroundCssClass="modalBackground" DropShadow="false" PopupDragHandleControlID="pnlContenidos">
+    </ajaxToolkit:ModalPopupExtender>
+    <asp:Panel ID="pnlContenidos" runat="server" Width="700px" Height="550px" Style="display: none;
+        text-align: left" BorderStyle="Groove" CssClass="CajaDialogo">
+        <uc1:InformeIndicador ID="InformeIndicador1" runat="server"></uc1:InformeIndicador>
+    </asp:Panel>
+</asp:Content>
---
+++ trunk/Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_UI/Private/Monitoreo/IndicadoresGenerales.aspx.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EDUAR_UI.Shared;
+using EDUAR_BusinessLogic.Common;
+using EDUAR_Entities;
+using EDUAR_Utility.Enumeraciones;
+using EDUAR_Entities.Reports;
+
+namespace EDUAR_UI
+{
+    public partial class IndicadoresGenerales : EDUARBasePage
+    {
+        #region --[Propiedades]--
+        /// <summary>
+        /// Gets or sets the lista cursos.
+        /// </summary>
+        /// <value>
+        /// The lista cursos.
+        /// </value>
+        public List<Curso> listaCursos
+        {
+            get
+            {
+                if (ViewState["listaCursos"] == null && cicloLectivoActual != null)
+                {
+                    BLCicloLectivo objBLCicloLectivo = new BLCicloLectivo();
+
+                    Asignatura objFiltro = new Asignatura();
+                    objFiltro.curso.cicloLectivo = cicloLectivoActual;
+                    if (User.IsInRole(enumRoles.Docente.ToString()))
+                        //nombre del usuario logueado
+                        objFiltro.docente.username = User.Identity.Name;
+                    listaCursos = objBLCicloLectivo.GetCursosByAsignatura(objFiltro);
+                    listaCursos.Sort((emp1, emp2) => emp1.nombre.CompareTo(emp2.nombre));
+                }
+                return (List<Curso>)ViewState["listaCursos"];
+            }
+            set { ViewState["listaCursos"] = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the lista indicadores.
+        /// </summary>
+        /// <value>
+        /// The lista indicadores.
+        /// </value>
+        public List<EDUAR_Entities.Reports.Indicador> listaIndicadores
+        {
+            get
+            {
+                if (ViewState["listaIndicadores"] == null && cicloLectivoActual != null)
+                {
+                    EDUAR_BusinessLogic.Reports.BLIndicador objBLIndicador = new EDUAR_BusinessLogic.Reports.BLIndicador();
+                    listaIndicadores = objBLIndicador.GetIndicadores(null);
+                }
+                return (List<EDUAR_Entities.Reports.Indicador>)ViewState["listaIndicadores"];
+            }
+            set { ViewState["listaIndicadores"] = value; }
+        }
+
+        #endregion
+
+        #region --[Eventos]--
+        /// <summary>
+        /// Método que se ejecuta al dibujar los controles de la página.
+        /// Se utiliza para gestionar las excepciones del método Page_Load().
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+            if (AvisoMostrar)
+            {
+                AvisoMostrar = false;
+
+                try
+                {
+                    Master.ManageExceptions(AvisoExcepcion);
+                }
+                catch (Exception ex) { Master.ManageExceptions(ex); }
+            }
+        }
+
+        /// <summary>
+        /// Handles the Load event of the Page control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                InformeIndicador1.SalirClick += (this.btnInformeIndicadorSalir_Click);
+
+                Master.BotonAvisoAceptar += (VentanaAceptar);
+
+                foreach (DataListItem item in dtlCursos.Controls)
+                {
+                    foreach (Control itemControl in item.Controls)
+                    {
+                        UserControls.Indicador indi = new UserControls.Indicador();
+
+                        if (itemControl.GetType().BaseType == indi.GetType())
+                        {
+                            indi = (UserControls.Indicador)itemControl;
+                            indi.SetEventoClick(this.btnInformeIndicador_Click);
+                            break;
+                        }
+                    }
+                }
+
+                if (!IsPostBack)
+                {
+                    //CargarCurso();
+
+                    dtlIndicadores.DataSource = listaIndicadores;
+                    dtlIndicadores.DataBind();
+                    udpIndicadores.Update();
+                    divCursos.Visible = false;
+                    udpCursos.Update();
+                }
+            }
+            catch (Exception ex)
+            {
+                AvisoMostrar = true;
+                AvisoExcepcion = ex;
+            }
+        }
+
+        /// <summary>
+        /// Ventanas the aceptar.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        void VentanaAceptar(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Master.ManageExceptions(ex);
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnInformeIndicador control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void btnInformeIndicador_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mpeContenido.Show();
+                EDUAR_UI.UserControls.Indicador indi = null;
+                Button btn = (Button)sender;
+                bool salir = false;
+                foreach (DataListItem item in dtlCursos.Controls)
+                {
+                    foreach (Control itemControl in item.Controls)
+                    {
+                        indi = new UserControls.Indicador();
+                        if (itemControl.GetType().BaseType == indi.GetType())
+                        {
+                            indi = (UserControls.Indicador)itemControl;
+                            if (indi.idCursoIndicador == ((EDUAR_UI.UserControls.Indicador)(((System.Web.UI.Control)(sender)).TemplateControl)).idCursoIndicador)
+                                salir = true;
+                            break;
+                        }
+                    }
+                    if (salir) break;
+
+                }
+
+                if (indi != null)
+                {
+                    InformeIndicador1.Titulo = indi.Título;
+                    InformeIndicador1.SP = "DatosIndicador_" + indi.nombreSP;
+                    InformeIndicador1.Hasta = DateTime.Today;
+                    InformeIndicador1.idCursoIndicador = indi.idCursoIndicador;
+
+                    if (btn.CommandName == "Principal")
+                        InformeIndicador1.Desde = DateTime.Today.AddDays(indi.HastaPrincipal * -1);
+                    else if (btn.CommandName == "Intermedio")
+                        InformeIndicador1.Desde = DateTime.Today.AddDays(indi.HastaIntermedio * -1);
+                    else if (btn.CommandName == "Secundario")
+                        InformeIndicador1.Desde = DateTime.Today.AddDays(indi.HastaSecundario * -1);
+
+                    InformeIndicador1.Show();
+                }
+            }
+            catch (Exception ex)
+            {
+                Master.ManageExceptions(ex);
+            }
+        }
+
+        /// <summary>
+        /// Handles the ItemCommand event of the dtlIndicadores control.
+        /// </summary>
+        /// <param name="source">The source of the event.</param>
+        /// <param name="e">The <see cref="DataListCommandEventArgs"/> instance containing the event data.</param>
+        protected void dtlIndicadores_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            try
+            {
+                int idIndicador = 0;
+                int.TryParse(e.CommandArgument.ToString(), out idIndicador);
+                if (e.CommandName == "Indicador")
+                {
+                    lblIndicador.Text = ((LinkButton)(e.CommandSource)).Text;
+                    dtlCursos.DataSource = listaCursos;
+                    dtlCursos.DataBind();
+
+                    CargarIndicador(idIndicador);
+
+                    divCursos.Visible = true;
+                    udpCursos.Update();
+                }
+            }
+            catch (Exception ex)
+            {
+                Master.ManageExceptions(ex);
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnInformeIndicadorSalir control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void btnInformeIndicadorSalir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                InformeIndicador1.Hide();
+                mpeContenido.Hide();
+            }
+            catch (Exception ex)
+            {
+                Master.ManageExceptions(ex);
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnVolverAnterior control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void btnVolverAnterior_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                base.idCursoCicloLectivo = 0;
+                base.cursoActual = new CursoCicloLectivo();
+                Response.Redirect("~/Private/AccesoCursos.aspx", false);
+            }
+            catch (Exception ex)
+            {
+                Master.ManageExceptions(ex);
+            }
+        }
+        #endregion
+
+        #region --[Métodos Privados]--
+        /// <summary>
+        /// Cargars the indicador.
+        /// </summary>
+        /// <param name="idIndicador">The id indicador.</param>
+        private void CargarIndicador(int idIndicador)
+        {
+            UserControls.Indicador indi = null;
+            Indicador miIndicador = listaIndicadores.Find(p => p.idIndicador == idIndicador);
+
+            foreach (DataListItem item in dtlCursos.Controls)
+            {
+                foreach (Control itemControl in item.Controls)
+                {
+                    indi = new UserControls.Indicador();
+                    if (itemControl.GetType().BaseType == indi.GetType())
+                    {
+                        indi = (UserControls.Indicador)itemControl;
+                        if (indi != null)
+                        {
+                            indi.SetEventoClick(this.btnInformeIndicador_Click);
+
+                            indi.Visible = true;
+                            indi.InvertirEscala = Convert.ToBoolean(miIndicador.invertirEscala.ToString());
+
+                            indi.HastaPrincipal = miIndicador.diasHastaPrincipal;
+                            if (DateTime.Today.Subtract(cicloLectivoActual.fechaInicio).Days < indi.HastaPrincipal)
+                                indi.HastaPrincipal = DateTime.Today.Subtract(cicloLectivoActual.fechaInicio).Days;
+
+                            indi.HastaIntermedio = miIndicador.diasHastaIntermedio;
+                            if (DateTime.Today.Subtract(cicloLectivoActual.fechaInicio).Days < indi.HastaIntermedio)
+                                indi.HastaIntermedio = DateTime.Today.Subtract(cicloLectivoActual.fechaInicio).Days;
+
+                            indi.HastaSecundario = miIndicador.diasHastaSecundario;
+                            if (DateTime.Today.Subtract(cicloLectivoActual.fechaInicio).Days < indi.HastaSecundario)
+                                indi.HastaSecundario = DateTime.Today.Subtract(cicloLectivoActual.fechaInicio).Days;
+
+                            indi.VerdePrincipal = miIndicador.verdeNivelPrincipal;
+                            indi.RojoPrincipal = miIndicador.rojoNivelPrincipal;
+                            indi.VerdeIntermedio = miIndicador.verdeNivelIntermedio;
+                            indi.RojoSecundario = miIndicador.rojoNivelIntermedio;
+                            indi.VerdeSecundario = miIndicador.verdeNivelSecundario;
+                            indi.RojoSecundario = miIndicador.rojoNivelSecundario;
+                            indi.nombreSP = miIndicador.nombreSP;
+                            indi.Título = indi.cursoIndicador + " - " + miIndicador.nombre;
+                            //indi.Título = indi.cursoIndicador;
+                            indi.CargarIndicador();
+                            break;
+                        }
+                    }
+                }
+            }
+            udpCursos.Update();
+        }
+        #endregion
+    }
+}
