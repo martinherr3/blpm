2183b494a42bae10c28fdcb044973ca73dd10cc1
belubzn <belubzn@gmail.com>
2013-05-20 22:38:26 +0000

    FinalBelu de Simulacion

---
+++ SimmulacionAlfombre/src/Objetos/AlfombraPrincipal.java
@@ -0,0 +1,197 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author Belén
+ */
+public class AlfombraPrincipal {
+
+    private double Reloj;
+    private String Evento;
+    private double RND;
+    private double LLegaProxPers;
+ 
+    private double finLimpieza;
+    private double esHoraSuspender;
+    private int NroPersProxFinLanz;
+    private double FinProxLanz;
+    private int CantPersEnLanz;
+    private String  EstadoAlfombra;
+    private int MaxCola;
+    private double MaxTiempoDEspera; 
+
+    public double getReloj() {
+        return Reloj;
+    }
+
+    public void setReloj(double Reloj) {
+        this.Reloj = Reloj;
+    }
+
+    public String getEvento() {
+        return Evento;
+    }
+
+    public void setEvento(String Evento) {
+        this.Evento = Evento;
+    }
+
+    public double getRND() {
+        return RND;
+    }
+
+    public void setRND(double RND) {
+        this.RND = RND;
+    }
+
+    public double getLLegaProxPers() {
+        return LLegaProxPers;
+    }
+
+    public void setLLegaProxPers(double LLegaProxPers) {
+        this.LLegaProxPers = LLegaProxPers;
+    }
+
+ 
+
+
+    public double getFinLimpieza() {
+        return finLimpieza;
+    }
+
+    public void setFinLimpieza(double finLimpieza) {
+        this.finLimpieza = finLimpieza;
+    }
+
+    public double getEsHoraSuspender() {
+        return esHoraSuspender;
+    }
+
+    public void setEsHoraSuspender(double esHoraSuspender) {
+        this.esHoraSuspender = esHoraSuspender;
+    }
+
+    public int getNroPersProxFinLanz() {
+        return NroPersProxFinLanz;
+    }
+
+    public void setNroPersProxFinLanz(int NroPersProxFinLanz) {
+        this.NroPersProxFinLanz = NroPersProxFinLanz;
+    }
+
+    public double getFinProxLanz() {
+        return FinProxLanz;
+    }
+
+    public void setFinProxLanz(double FinProxLanz) {
+        this.FinProxLanz = FinProxLanz;
+    }
+
+    public int getCantPersEnLanz() {
+        return CantPersEnLanz;
+    }
+
+    public void setCantPersEnLanz(int CantPersEnLanz) {
+        this.CantPersEnLanz = CantPersEnLanz;
+    }
+
+    public String getEstadoAlfombra() {
+        return EstadoAlfombra;
+    }
+
+    public void setEstadoAlfombra(String EstadoAlfombra) {
+        this.EstadoAlfombra = EstadoAlfombra;
+    }
+
+    public int getMaxCola() {
+        return MaxCola;
+    }
+
+    public void setMaxCola(int MaxCola) {
+        this.MaxCola = MaxCola;
+    }
+
+    public double getMaxTiempoDEspera() {
+        return MaxTiempoDEspera;
+    }
+
+    public void setMaxTiempoDEspera(double MaxTiempoDEspera) {
+        this.MaxTiempoDEspera = MaxTiempoDEspera;
+    }
+
+    public AlfombraPrincipal() {
+    }
+
+    public AlfombraPrincipal(double Reloj, String Evento, double RND, double LLegaProxPers, double finLimpieza, double esHoraSuspender, int NroPersProxFinLanz, double FinProxLanz, int CantPersEnLanz, String EstadoAlfombra, int MaxCola, double MaxTiempoDEspera) {
+        this.Reloj = Reloj;
+        this.Evento = Evento;
+        this.RND = RND;
+        this.LLegaProxPers = LLegaProxPers;
+       
+        this.finLimpieza = finLimpieza;
+        this.esHoraSuspender = esHoraSuspender;
+        this.NroPersProxFinLanz = NroPersProxFinLanz;
+        this.FinProxLanz = FinProxLanz;
+        this.CantPersEnLanz = CantPersEnLanz;
+        this.EstadoAlfombra = EstadoAlfombra;
+        this.MaxCola = MaxCola;
+        this.MaxTiempoDEspera = MaxTiempoDEspera;
+    }
+
+    public void getMaxCola(Integer integer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getCantPersEnLanz(Integer integer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getFinProxLanz(Double aDouble) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getNroPersProxFinLanz(Integer integer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getEsHoraSuspender(Double aDouble) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getFinLimpieza(Integer integer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getEsHoraLimpieza(Double aDouble) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getReloj(Double aDouble) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getEvento(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getRND(Double aDouble) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getLLegaProxPers(Double aDouble) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getEstadoAlfombra(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getMaxTiempoDEspera(Double aDouble) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
