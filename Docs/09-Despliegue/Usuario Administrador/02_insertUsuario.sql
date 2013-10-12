af3e98598e84876787df8f50a74879a15c4eec1d
king.orco <king.orco@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2013-10-12 03:22:25 +0000

    Scripts de carga del Usuario Administrador tanto en EDUAR como en ASPNET_Services (se indica secuencia)

---
+++ Docs/09-Despliegue/Usuario Administrador/02_insertUsuario.sql
@@ -0,0 +1,10 @@
+USE [EDUAR_aspnet_services]
+
+INSERT INTO  ([ApplicationId], [UserId], [UserName], [LoweredUserName], [MobileAlias], [IsAnonymous], [LastActivityDate]) VALUES ('340DD3A1-F04E-427C-8851-07EBECA2417A', 
+'379AA002-D108-4F26-9597-24CE888FFAEB', 
+'eduar.administrador', 
+'eduar.administrador', 
+NULL, 
+'0', 
+NULL);
+GO
