21b429239d14a1d6b2307000e746ebf4ebea83e6
king.orco <king.orco@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2013-10-12 03:22:25 +0000

    Scripts de carga del Usuario Administrador tanto en EDUAR como en ASPNET_Services (se indica secuencia)

---
+++ trunk/Docs/09-Despliegue/Usuario Administrador/01_insertPersona.sql
@@ -0,0 +1,21 @@
+USE [EDUAR]
+INSERT INTO  ([idPersona], [nombre], [apellido], [numeroDocumento], [idTipoDocumento], [domicilio], [barrio], [idLocalidad], [sexo], [fechaNacimiento], [telefonoFijo], [telefonoCelular], [telefonoCelularAlternativo], [email], [activo], [username], [idTipoPersona]) 
+VALUES (
+'1', 
+'Juan Carlos', 
+'Administrador', 
+'32111222', 
+'7', 
+'Marcelo T de Alvear 540', 
+'', 
+'6', 
+'M', 
+'1984-10-13', 
+NULL, 
+NULL, 
+NULL, 
+'eduar.administrador@eduar2.com', 
+'1', 
+'eduar.administrador', 
+'4');
+GO
