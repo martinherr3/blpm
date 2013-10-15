c8f853fe76540b8c72319f109ff6a9f4cbea9419
laupastorino@gmail.com <laupastorino@gmail.com@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2013-10-15 23:50:59 +0000

    Lau - Correcciones SCRIPTS

--- trunk/Docs/09-Despliegue/Creación estructura/User Creation.sql
+++ trunk/Docs/09-Despliegue/Creación estructura/User Creation.sql
@@ -1,15 +1,56 @@
--- Crea al login EduarAdmin con password 'admin2013'.
-CREATE LOGIN EduarAdmin 
-    WITH PASSWORD = 'admin2013';
+USE [master]
+GO
+EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2
+GO
+GO
+CREATE LOGIN [EduarAdmin] WITH PASSWORD=N'admin2013', DEFAULT_DATABASE=[EDUAR], DEFAULT_LANGUAGE=[Español], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO 
+
+ALTER LOGIN [EduarAdmin] ENABLE
 GO
 
--- Crea un usuario de la BD para el login creado anteriormente.
-CREATE USER EduarAdmin FOR LOGIN EduarAdmin;
+ALTER SERVER ROLE [sysadmin] ADD MEMBER [EduarAdmin]
 GO
+
+ALTER SERVER ROLE [securityadmin] ADD MEMBER [EduarAdmin]
+GO
+
+ALTER SERVER ROLE [serveradmin] ADD MEMBER [EduarAdmin]
+GO
+
+ALTER SERVER ROLE [setupadmin] ADD MEMBER [EduarAdmin]
+GO
+
+ALTER SERVER ROLE [processadmin] ADD MEMBER [EduarAdmin]
+GO
+
+ALTER SERVER ROLE [diskadmin] ADD MEMBER [EduarAdmin]
+GO
+
+ALTER SERVER ROLE [dbcreator] ADD MEMBER [EduarAdmin]
+GO
+
+ALTER SERVER ROLE [bulkadmin] ADD MEMBER [EduarAdmin]
+GO
+
+USE [EDUAR]
+GO
+CREATE USER [EduarAdmin] FOR LOGIN [EduarAdmin]
+GO 
 
 -- Le asigna el rol de owner
-EXEC sp_addrolemember 'db_owner', 'UsuarioAdmin';
+EXEC sp_addrolemember 'db_owner', 'EduarAdmin';
 
 -- Le asigna el owner creado a la base de datos
-EXEC [EDUAR].dbo.sp_changedbowner N'UsuarioAdmin'
+--EXEC [EDUAR].dbo.sp_changedbowner N'EduarAdmin'
 
+USE [EDUAR_aspnet_services]
+GO
+CREATE USER [EduarAdmin] FOR LOGIN [EduarAdmin]
+GO 
+
+-- Le asigna el rol de owner
+EXEC sp_addrolemember 'db_owner', 'EduarAdmin';
+
+-- Le asigna el owner creado a la base de datos
+--EXEC [EDUAR].dbo.sp_changedbowner N'EduarAdmin'
