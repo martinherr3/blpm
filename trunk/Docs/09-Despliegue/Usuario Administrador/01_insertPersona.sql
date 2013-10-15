4463b4ba992beeaf7ee5f3369e4b12a642ccbe22
laupastorino@gmail.com <laupastorino@gmail.com@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2013-10-15 03:36:15 +0000

    Lau - Correcciones SCRIPTS

--- trunk/Docs/09-Despliegue/Usuario Administrador/01_insertPersona.sql
+++ trunk/Docs/09-Despliegue/Usuario Administrador/01_insertPersona.sql
@@ -1,14 +1,20 @@
 USE [EDUAR]
-INSERT INTO  ([idPersona], [nombre], [apellido], [numeroDocumento], [idTipoDocumento], [domicilio], [barrio], [idLocalidad], [sexo], [fechaNacimiento], [telefonoFijo], [telefonoCelular], [telefonoCelularAlternativo], [email], [activo], [username], [idTipoPersona]) 
+
+SET IDENTITY_INSERT [EDUAR].[dbo].Personas ON
+
+ALTER TABLE Personas NOCHECK CONSTRAINT FK_Personas_Localidades
+ALTER TABLE Personas NOCHECK CONSTRAINT FK_Personas_TiposDocumentos
+
+INSERT INTO Personas ([idPersona], [nombre], [apellido], [numeroDocumento], [idTipoDocumento], [domicilio], [barrio], [idLocalidad], [sexo], [fechaNacimiento], [telefonoFijo], [telefonoCelular], [telefonoCelularAlternativo], [email], [activo], [username], [idTipoPersona]) 
 VALUES (
 '1', 
 'Juan Carlos', 
 'Administrador', 
 '32111222', 
-'7', 
+1, 
 'Marcelo T de Alvear 540', 
 '', 
-'6', 
+1, 
 'M', 
 '1984-10-13', 
 NULL, 
@@ -19,3 +25,7 @@ NULL,
 'eduar.administrador', 
 '4');
 GO
+
+ALTER TABLE Personas CHECK CONSTRAINT FK_Personas_Localidades
+ALTER TABLE Personas CHECK CONSTRAINT FK_Personas_TiposDocumentos 
+SET IDENTITY_INSERT [EDUAR].[dbo].Personas OFF
--- trunk/Docs/09-Despliegue/Usuario Administrador/02_insertUsuario.sql
+++ trunk/Docs/09-Despliegue/Usuario Administrador/02_insertUsuario.sql
@@ -1,10 +1,10 @@
 USE [EDUAR_aspnet_services]
 
-INSERT INTO  ([ApplicationId], [UserId], [UserName], [LoweredUserName], [MobileAlias], [IsAnonymous], [LastActivityDate]) VALUES ('340DD3A1-F04E-427C-8851-07EBECA2417A', 
+INSERT INTO aspnet_Users ([ApplicationId], [UserId], [UserName], [LoweredUserName], [MobileAlias], [IsAnonymous], [LastActivityDate]) VALUES ('340DD3A1-F04E-427C-8851-07EBECA2417A', 
 '379AA002-D108-4F26-9597-24CE888FFAEB', 
 'eduar.administrador', 
 'eduar.administrador', 
 NULL, 
-'0', 
-NULL);
+0, 
+GETDATE());
 GO
