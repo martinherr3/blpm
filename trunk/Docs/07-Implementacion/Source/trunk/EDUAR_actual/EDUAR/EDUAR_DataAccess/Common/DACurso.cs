b7a7ff9701e52e6ce45c17c2389439e49b82826f
laupastorino@gmail.com <laupastorino@gmail.com@b31a8bf0-e33e-bb18-16e9-729a5acef077>
2013-12-01 02:25:30 +0000

    Lau - cambios de último momento!

--- trunk/Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_DataAccess/Common/DACurso.cs
+++ trunk/Docs/07-Implementacion/Source/trunk/EDUAR_actual/EDUAR/EDUAR_DataAccess/Common/DACurso.cs
@@ -163,7 +163,48 @@ namespace EDUAR_DataAccess.Common
                 if (nivel != null && nivel.idNivel > 0)
                     Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idNivel", DbType.Int32, nivel.idNivel);
 
+                
+                IDataReader reader = Transaction.DataBase.ExecuteReader(Transaction.DBcomand);
 
+                CursoCicloLectivo objCurso = null;
+                List<CursoCicloLectivo> listaCursos = new List<CursoCicloLectivo>();
+                while (reader.Read())
+                {
+                    objCurso = new CursoCicloLectivo();
+
+                    objCurso.idCursoCicloLectivo = Convert.ToInt32(reader["idCursoCicloLectivo"]);
+                    objCurso.curso.nombre = reader["nombre"].ToString();
+
+                    listaCursos.Add(objCurso);
+                }
+                return listaCursos;
+            }
+            catch (SqlException ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - GetCursosCicloLectivo()", ClassName),
+                                    ex, enuExceptionType.SqlException);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomizedException(string.Format("Fallo en {0} - GetCursosCicloLectivo()", ClassName),
+                                    ex, enuExceptionType.DataAccesException);
+            }
+        }
+
+        public List<CursoCicloLectivo> GetCursosCicloLectivo(Curso curso, CicloLectivo cicloLectivo)
+        {
+            try
+            {
+                Transaction.DBcomand = Transaction.DataBase.GetStoredProcCommand("CursosCicloLectivo_Select");
+                if (cicloLectivo != null && cicloLectivo.idCicloLectivo > 0)
+                    Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idCicloLectivo", DbType.Int32, cicloLectivo.idCicloLectivo);
+                if (curso != null)
+                {
+                    if (curso.nivel != null && curso.nivel.idNivel > 0)
+                        Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@idNivel", DbType.Int32, curso.nivel.idNivel);
+                    if (curso.nombre != null && !string.IsNullOrEmpty(curso.nombre))
+                        Transaction.DataBase.AddInParameter(Transaction.DBcomand, "@username", DbType.String, curso.nombre);
+                }
                 IDataReader reader = Transaction.DataBase.ExecuteReader(Transaction.DBcomand);
 
                 CursoCicloLectivo objCurso = null;
@@ -191,5 +232,6 @@ namespace EDUAR_DataAccess.Common
             }
         }
         #endregion
+
     }
 }
